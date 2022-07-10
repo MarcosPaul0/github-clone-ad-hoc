10f31780d336ee2c4af896407bf85840caf7f9a9
MarcosPaulo <marcosphip7@gmail.com>
2022-07-09 21:44:43 -0300

    tela principal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "react-query";
+import { RepositoriesContextProvider } from "../contexts/RepositoriesContext";
+import { queryClient } from '../services/query';
 
 import "../styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RepositoriesContextProvider>
+        <Component {...pageProps} />
+      </RepositoriesContextProvider>
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,45 @@
 import Head from "next/head";
+import { useState } from "react";
+import { Dashboard } from "../components/Dashboard";
+import { Header } from "../components/Header";
+import { RepositoryList } from "../components/RepositoryList";
+import { Sidebar } from "../components/Sidebar";
+
+import styles from "../styles/app.module.css";
 
 export default function Home() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [selected, setSelected] = useState<"table" | "dashboard">("table");
+
+  function handleSelect(option: "table" | "dashboard") {
+    setSelected(option);
+  }
+
+  function handleToggleSidebar() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div>
+    <>
       <Head>
         <title>Github Clone | Ad Hoc</title>
       </Head>
 
-      <main>
-        <h1 className="text-red-500">Hello World</h1>
-      </main>
-    </div>
+      <div className={styles.container}>
+        <Header
+          handleToggleSidebar={handleToggleSidebar}
+          handleSelect={handleSelect}
+          selected={selected}
+        />
+        <main
+          className={`bg-gray-800 text-gray-50 flex w-full relative ${styles.content}`}
+        >
+          <Sidebar isOpen={isOpen} />
+
+          {selected === "table" ? <RepositoryList /> : <Dashboard />}
+        </main>
+      </div>
+    </>
   );
-};
+}
